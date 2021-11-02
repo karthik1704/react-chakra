5463e4af593efb94fe043d42a6cfeb0a6e19488c
Karthik A <karthikthee7@gmail.com>
2021-11-02 20:45:26 +0530

    add small animation

--- src/Pages/PageNotFound/PageNotFound.tsx
+++ src/Pages/PageNotFound/PageNotFound.tsx
@@ -1,16 +1,28 @@
-import { FC, ReactElement } from 'react'
+import { FC, ReactElement, useEffect } from 'react'
 
-import { Button, Flex, Heading } from '@chakra-ui/react'
+import { Button, Flex, Heading, Slide, useDisclosure } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 
 const PageNotFound: FC = (): ReactElement => {
+  const { isOpen, onToggle } = useDisclosure()
+  useEffect(() => {
+    onToggle()
+  }, [])
   return (
-    <Flex align="center" justify="center" minH="100vh" direction="column">
-      <Heading mb={2}>OOPS! I can't find page you looking for 😵😵😵</Heading>
-      <Button colorScheme="purple" variant="solid" size="lg" as={Link} to="..">
-        Go Back
-      </Button>
-    </Flex>
+    <Slide direction="bottom" in={isOpen} unmountOnExit>
+      <Flex align="center" justify="center" minH="100vh" direction="column">
+        <Heading mb={2}>OOPS! I can't find page you looking for 😵😵😵</Heading>
+        <Button
+          colorScheme="purple"
+          variant="solid"
+          size="lg"
+          as={Link}
+          to=".."
+        >
+          Go Back
+        </Button>
+      </Flex>
+    </Slide>
   )
 }
 
