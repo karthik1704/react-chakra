e8d088c6bc4c460cbc765ba21a41129ed6e74296
Karthik A <karthikthee7@gmail.com>
2021-11-02 20:32:38 +0530

    Add AppRoutes to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import { useState } from "react";
+import { useState } from 'react'
 
-import { Box } from "@chakra-ui/react";
+import { Box } from '@chakra-ui/react'
 
-import NavBar from "./components/NavBar/NavBar";
+import NavBar from './components/NavBar/NavBar'
+import Footer from './components/Footer/Footer'
+import { BrowserRouter } from 'react-router-dom'
+import AppRoutes from './Routes/AppRoutes'
 
 function App() {
   return (
     <Box>
-      <NavBar />
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
     </Box>
-  );
+  )
 }
 
-export default App;
+export default App
