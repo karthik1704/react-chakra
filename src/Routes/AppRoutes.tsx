9c838190eb32f9a4f0bd13c9bcda0b372766ea4c
Karthik A <karthikthee7@gmail.com>
2021-11-03 20:57:12 +0530

    Add About Page to Routes

--- src/Routes/AppRoutes.tsx
+++ src/Routes/AppRoutes.tsx
@@ -5,16 +5,21 @@ import AppLayout from '../Layout/AppLayout'
 import AuthLayout from '../Layout/AuthLayout'
 import Home from '../Pages/Home/Home'
 import Login from '../Pages/Login/Login'
+import Register from '../Pages/Register/Register'
+
 import PageNotFound from '../Pages/PageNotFound/PageNotFound'
+import About from '../Pages/About/About'
 
 const AppRoutes = (): ReactElement => {
   return (
     <Routes>
       <Route path="/" element={<AppLayout />}>
         <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
       </Route>
       <Route element={<AuthLayout />}>
         <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
       </Route>
       <Route path="*" element={<PageNotFound />} />
     </Routes>
