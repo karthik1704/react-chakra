b49f894c00f60816c11be5a537f0479a11ea1981
Karthik A <karthikthee7@gmail.com>
2021-11-04 11:47:40 +0530

    add redux store

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 
-import "./index.css";
-import App from "./App";
-import theme from "./theme";
+import { Provider } from 'react-redux'
+
+import { store } from './features/store'
+
+import './index.css'
+import App from './App'
+import theme from './theme'
 
 ReactDOM.render(
   <React.StrictMode>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <Provider store={store}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
