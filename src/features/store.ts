80dc62433975a18f79c3d96e1a8a2bd49b37af21
Karthik A <karthikthee7@gmail.com>
2021-11-05 20:26:05 +0530

    add authentication reducer to store

--- src/features/store.ts
+++ src/features/store.ts
@@ -1,6 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+import authenticationReducer from './Authentication/authenticationSlice'
+import { authenticationApi } from './Authentication/authenticationApi'
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    authentication: authenticationReducer,
+    [authenticationApi.reducerPath]: authenticationApi.reducer,
+  },
   middleware: (getDefaultMiddeware) => getDefaultMiddeware().concat(),
 })
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
